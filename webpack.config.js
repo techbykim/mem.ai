6d287654017e167953b48bffe028b39bc4acf28f
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2023-06-26 15:22:19 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    popup: path.join(__dirname, 'src', 'index.jsx'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
